e2fa28f38d2f7b5fa8bc58a4b0e012171b22cfb7
Simon Elvery <simon@elvery.net>
2026-02-16 16:02:39 +1000

    Get puppeteer working again

--- src/lib/browser.server.js
+++ src/lib/browser.server.js
@@ -1,25 +1,9 @@
-import { env } from '$env/dynamic/private';
-import chromium from '@sparticuz/chromium-min';
-import puppeteer from 'puppeteer-core';
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
+import { env } from "$env/dynamic/private";
 
-async function getBrowser() {
-
-  const isLocal = env.IS_LOCAL === '1';
-
-  const args = [...(isLocal? puppeteer.defaultArgs(): chromium.args), '--hide-scrollbars', '--disable-web-security'];
-  const defaultViewport = chromium.defaultViewport;
-  const executablePath = isLocal ? env.BROWSER_PATH : await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v130.0.0/chromium-v130.0.0-pack.tar`
-    )
-    console.log('executablePath :>> ', executablePath);
-  return puppeteer.launch({
-    args,
-    defaultViewport,
-    executablePath,
-    headless: isLocal ? false : chromium.headless,
-    // ignoreHTTPSErrors: true,
-  });
-}
+const isLocal = env.IS_LOCAL === '1';
+const executablePath = isLocal ? env.BROWSER_PATH : await chromium.executablePath()
 
 /**
  *
@@ -28,14 +12,22 @@ async function getBrowser() {
  * @param {number} [width] Set the viewport width before taking screenshot (default: `810`)
  * @param {number} [height] Set the viewport height before taking screenshot (default: `1080`)
  */
-export async function getScreenshot(url, selector = 'body', width = 810, height = 1080 ) {
-  const viewport = { width, height, deviceScaleFactor: 2 };
-  const browser = await getBrowser();
-  const page = await browser.newPage();
-  await page.setViewport(viewport);
-  await page.goto(url, { waitUntil: "networkidle0" });
-  const el = await page.waitForSelector(selector);
-  const file = await el?.screenshot();
-  return file;
-}
+export async function getScreenshot(url, selector = 'body', width = 810, height = 1080) {
 
+  const browser = await puppeteer.launch({
+    args: puppeteer.defaultArgs({ args: isLocal ? [] : chromium.args, headless: "shell" }),
+    defaultViewport: {width, height, deviceScaleFactor: 2},
+    executablePath,
+    headless: "shell",
+  });
+
+  try {
+    const page = await browser.newPage();
+    await page.goto(url);
+    const el = await page.waitForSelector(selector);
+    const file = await el?.screenshot();
+    return file;
+  } finally {
+    await browser.close();
+  }
+}
--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,27 +1,23 @@
-
 import { getScreenshot } from '$lib/browser.server.js';
 import { error } from '@sveltejs/kit';
 
-export const config = {
-  maxDuration: 60
-}
+export const config = { maxDuration: 60 };
 
-export let GET = async ({url}) => {
+export let GET = async ({ url }) => {
+	const target = url.searchParams.get('url');
+	const selector = url.searchParams.get('selector') || 'body';
+	const width = +(url.searchParams.get('width') || '0') || undefined;
+	const height = +(url.searchParams.get('height') || '0') || undefined;
 
-  const target = url.searchParams.get('url');
-  const selector = url.searchParams.get('selector') || 'body';
-  const width = +(url.searchParams.get('width') || '0') || undefined;
-  const height = +(url.searchParams.get('width') || '0') || undefined;
+	if (!target) {
+		return error(404, 'URL must be provided.');
+	}
 
-  if (!target) {
-    return error(404, "URL must be provided.");
-  }
+	const screenshot = await getScreenshot(target, selector, width, height);
 
-  const screenshot = await getScreenshot(target, selector, width, height);
+	if (!screenshot) {
+		return error(500, 'Failed to generate screenshot.');
+	}
 
-  if (!screenshot) {
-    return error(500, 'Failed to generate screenshot.');
-  }
-
-  return new Response(Buffer.from(screenshot));
-}
+	return new Response(Buffer.from(screenshot));
+};

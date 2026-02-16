c6e7f168c18513864b45487fb9f559dd9639dab7
Simon Elvery <simon@elvery.net>
2026-02-17 08:46:35 +1000

    Switch to chromium-min

--- src/lib/browser.server.js
+++ src/lib/browser.server.js
@@ -1,9 +1,13 @@
-import puppeteer from "puppeteer-core";
-import chromium from "@sparticuz/chromium";
-import { env } from "$env/dynamic/private";
+import puppeteer from 'puppeteer-core';
+import chromium from '@sparticuz/chromium-min';
+import { env } from '$env/dynamic/private';
 
 const isLocal = env.IS_LOCAL === '1';
-const executablePath = isLocal ? env.BROWSER_PATH : await chromium.executablePath()
+const executablePath = isLocal
+	? env.BROWSER_PATH
+	: await chromium.executablePath(
+			'https://github.com/Sparticuz/chromium/releases/download/v143.0.4/chromium-v143.0.4-pack.tar'
+		);
 
 /**
  *
@@ -13,21 +17,20 @@ const executablePath = isLocal ? env.BROWSER_PATH : await chromium.executablePat
  * @param {number} [height] Set the viewport height before taking screenshot (default: `1080`)
  */
 export async function getScreenshot(url, selector = 'body', width = 810, height = 1080) {
+	const browser = await puppeteer.launch({
+		args: puppeteer.defaultArgs({ args: isLocal ? [] : chromium.args, headless: 'shell' }),
+		defaultViewport: { width, height, deviceScaleFactor: 2 },
+		executablePath,
+		headless: 'shell'
+	});
 
-  const browser = await puppeteer.launch({
-    args: puppeteer.defaultArgs({ args: isLocal ? [] : chromium.args, headless: "shell" }),
-    defaultViewport: {width, height, deviceScaleFactor: 2},
-    executablePath,
-    headless: "shell",
-  });
-
-  try {
-    const page = await browser.newPage();
-    await page.goto(url);
-    const el = await page.waitForSelector(selector);
-    const file = await el?.screenshot();
-    return file;
-  } finally {
-    await browser.close();
-  }
+	try {
+		const page = await browser.newPage();
+		await page.goto(url);
+		const el = await page.waitForSelector(selector);
+		const file = await el?.screenshot();
+		return file;
+	} finally {
+		await browser.close();
+	}
 }
